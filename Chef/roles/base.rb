3bb77f92423dda4a4ef2b3866d80fae8ed2dc340
Andy Grunwald <andygrunwald@gmail.com>
2014-04-21 13:36:46 +0200

    Upgraded typo3analytics from 2.1.1 to 2.1.2 and PHP to 5.5

--- Chef/roles/base.rb
+++ Chef/roles/base.rb
@@ -2,11 +2,15 @@ name "base"
 description "The base role. Sets up basic stuff like apt, git etc."
 
 run_list "recipe[apt]",
+		 "recipe[dotdeb]",
 		 "recipe[build-essential]",
 		 "recipe[git]",
 		 "recipe[vim]"
 
 override_attributes(
+	:dotdeb => {
+		:php55 => true
+	},
 	:'build-essential' => {
 		:compile_time => true
 	}
